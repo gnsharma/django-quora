64fa7ca8db72c85d2ecdb38b5fad0cd71999a810
Govind <govind@Govind.local>
2018-03-21 15:53:37 +0530

    added few tests

--- quora/tests.py
+++ quora/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.utils import timezone
+
+from quora.models import Question
+
+
+def test_was_published_recently_with_future_question():
+    time = timezone.now() + datetime.timedelta(days=30)
+    future_question = Question(pub_date=time)
+    assert future_question.was_published_recently() is False
+
+
+def test_was_published_recently_with_old_question():
+    time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+    old_question = Question(pub_date=time)
+    assert old_question.was_published_recently() is False
+
+
+def test_was_published_recently_with_recent_question():
+    time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+    recent_question = Question(pub_date=time)
+    assert recent_question.was_published_recently() is True
+
