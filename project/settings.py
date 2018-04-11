a08bb607a1e3f189308af97d427e3d9616fd539c
Govind <govind@Govind.local>
2018-04-12 01:34:17 +0530

    removed

--- project/settings.py
+++ project/settings.py
@@ -158,5 +158,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'gnsharma0810@gmail.com'
-EMAIL_HOST_PASSWORD = 'project@1234'
+EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = 'Govind <gnsharma0810@gmail.com>'
